5b99bd33f4f400de13082515c4d1ebdbb5fddbc5
Towkir Ahmed <towkir17@gmail.com>
2016-08-24 04:24:27 +0600

    An apparant stable push, maybe final too.

--- banner_generator/magic/generator.js
+++ banner_generator/magic/generator.js
@@ -1,24 +1,42 @@
 
-var banner = document.getElementById("banner");
-var context = banner.getContext("2d");
-context.font = "50px Open Sans";
-
+//lets set the maximum length for the headline.
+document.getElementById("banner_heading").setAttribute("maxlength", "32");
+//now taking care of the canvases. firstly the background layer;
+var background = document.getElementById("banner_background");
+background_ctx = background.getContext("2d");
+//now the text layer;
+var textLayer = document.getElementById("banner");
+textLayer_ctx = textLayer.getContext("2d");
+textLayer_ctx.font = "300 350% Open Sans";
+//lets create an image element;
+var backgroundImage = new Image(background.width, background.height);
+backgroundImage.src = 'images/banner_plain.svg';
 //function that generates the banner preview from user input text;
 function previewBanner() {
-  context.clearRect(0, 0, banner.width, banner.height);
-  context.textAlign = "center";
-  context.fillText(document.getElementById("banner_heading").value, banner.width/2, banner.height/4);
-    
+  background_ctx.clearRect(0, 0, background.width, background.height); //      Clearing both layers
+  textLayer_ctx.clearRect(0, 0, textLayer.width, textLayer.height);  //           before preview
+  background_ctx.drawImage(backgroundImage, 0, 0, background.width, background.height);
+  textLayer_ctx.textAlign = "center";
+  textLayer_ctx.fillText(document.getElementById("banner_heading").value, textLayer.width/2, textLayer.height/4);
+  //now drwaing the textLayer canvas into the background canvas
+  background_ctx.drawImage(textLayer, 0, 0, background.width, background.height);
 }
-
+//Will it look good to lead the canvas totally white? Nope, show some default banner background;
+window.addEventListener("load", function(){
+  background_ctx.drawImage(backgroundImage, 0, 0, background.width, background.height);
+});
+//Calling the friendly previewBanner function;
 var preview = document.getElementById("preview");
 preview.addEventListener("click", previewBanner);
 
 //function that downloads the previously generated banner;
-function downloadImage() {
-  
+function downloadImage(link, canvasID, fileName) {
+  link.href = canvasID.toDataURL('image/png');
+  link.download = fileName;
 }
 
-
 var downloadButton = document.getElementById("download");
-downloadButton.addEventListener("click", downloadImage);
+downloadButton.addEventListener("click", function(){
+  previewBanner(); //downloading gets the exact current state by this;
+  downloadImage(this, background, "TestdayBanner.png");
+});
