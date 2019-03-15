5b81e28d2c2b4d888c73636e8c0ede00521a3e87
LiquidO2 <sayedibnmasud@gmail.com>
2019-03-16 03:47:27 +0600

    Upated to use current logos. Added channel specific logos and channel selection feature

--- magic/generator.js
+++ magic/generator.js
@@ -2,6 +2,8 @@
 //lets set the maximum length for the headline.
 document.getElementById("banner_heading").setAttribute("maxlength", "40");
 var channel =  document.getElementById("channel_name");
+
+// Channel selection
 channel.addEventListener("change", function(){
     if(channel.value == 1)
         logoImage.src = 'images/release.png';
@@ -9,35 +11,42 @@ channel.addEventListener("change", function(){
         logoImage.src = 'images/beta.png';
     else if(channel.value == 3)
         logoImage.src = 'images/nightly.png';
+    else if(channel.value == 4)
+        logoImage.src = 'images/dev.png';
 
 });
+
 //now taking care of the canvases. firstly the background layer;
 var background = document.getElementById("banner_background");
 background_ctx = background.getContext("2d");
+
+//lets create logo image element;
+var logoLayer = document.getElementById("fxlogo");
+logoLayer_ctx = logoLayer.getContext("2d");
+
 //now the text layer;
 var textLayer = document.getElementById("banner");
 textLayer_ctx = textLayer.getContext("2d");
 textLayer_ctx.font = "200 250% Open Sans";
+
 //lets create an image element;
 var backgroundImage = new Image(background.width, background.height);
 backgroundImage.src = 'images/banner_plain.svg';
-//lets create logo image element;
-var logoLayer = document.getElementById("fxlogo");
-logoLayer_ctx = logoLayer.getContext("2d");
+//lets create logo image
 var logoImage = new Image(background.height/2.67, background.height/2.67);
 logoImage.src = 'images/release.png';
 
-
 //function that generates the banner preview from user input text;
 function previewBanner() {
-  background_ctx.clearRect(0, 0, background.width, background.height); //      Clearing both layers
-  textLayer_ctx.clearRect(0, 0, textLayer.width, textLayer.height);  //           before preview
-  logoLayer_ctx.clearRect(0,0,logoLayer.width, textLayer.height);
+  background_ctx.clearRect(0, 0, background.width, background.height); //      Clearing all
+  textLayer_ctx.clearRect(0, 0, textLayer.width, textLayer.height);  //           layers
+  logoLayer_ctx.clearRect(0,0,logoLayer.width, textLayer.height); //          before preview
 
   background_ctx.drawImage(backgroundImage, 0, 0, background.width, background.height);
   logoLayer_ctx.drawImage(logoImage,105,110, logoLayer.height/2.2, logoLayer.height/2.2);
   textLayer_ctx.textAlign = "center";
   textLayer_ctx.fillText(document.getElementById("banner_heading").value, textLayer.width/2, textLayer.height/4);
+
   //now drwaing the textLayer canvas into the background canvas
   background_ctx.drawImage(textLayer, 0, 0, background.width, background.height);
   background_ctx.drawImage(logoLayer, 0, 0, background.width, background.height);
