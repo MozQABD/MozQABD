a3b039e3a37e28616b1ecc2bc5ffc9f851a79704
LiquidO2 <sayedibnmasud@gmail.com>
2019-03-17 17:28:23 +0600

    Real time preview of firefox logo and minor bug fixes.

--- magic/generator.js
+++ magic/generator.js
@@ -1,28 +1,9 @@
 
 //lets set the maximum length for the headline.
 document.getElementById("banner_heading").setAttribute("maxlength", "40");
+
+// getting channel value
 var channel =  document.getElementById("channel_name");
-
-// updatesfxlogo source based on channel selection
-function updatefxlogo(){
-    if(channel.value == 1){
-        fxLogoImage.src = 'images/release.png';
-    }
-    else if(channel.value == 2){
-        fxLogoImage.src = 'images/beta.png';
-    }
-    else if(channel.value == 3){
-        fxLogoImage.src = 'images/nightly.png';
-    }
-    else if(channel.value == 4){
-        fxLogoImage.src = 'images/dev.png';
-    }
-}
-
-channel.addEventListener("change", ()=>{
-    updatefxlogo();
-    setTimeout(previewBanner, 50); // update banner preview
-});
 
 //now taking care of the canvases. firstly the background layer;
 var background = document.getElementById("banner_background");
@@ -39,15 +20,23 @@ mozqabdLogoLayer_ctx = mozqabdLogoLayer.getContext("2d");
 //now the text layer;
 var textLayer = document.getElementById("banner");
 textLayer_ctx = textLayer.getContext("2d");
-textLayer_ctx.font = "400 250% Zilla Slab";
+textLayer_ctx.font = "350 300% Zilla Slab";
 
 //lets create a background image element;
 var backgroundImage = new Image(background.width, background.height);
 backgroundImage.src = 'images/banner_plain.svg';
 
-//lets create firefox logo image element
-var fxLogoImage = new Image(background.height, background.height);
-fxLogoImage.src = 'images/release.png';
+//lets create all firefox logo image element
+var fxLogoImage = [];
+fxLogoImage[0] = new Image(background.height, background.height);
+fxLogoImage[0].src = 'images/release.png';
+fxLogoImage[1] = new Image(background.height, background.height);
+fxLogoImage[1].src = 'images/beta.png';
+fxLogoImage[2] = new Image(background.height, background.height);
+fxLogoImage[2].src = 'images/nightly.png';
+fxLogoImage[3] = new Image(background.height, background.height);
+fxLogoImage[3].src = 'images/dev.png';
+
 
 //now create mozqabd logo image element
 var mozqabdLogoImage = new Image(background.height, background.height);
@@ -55,44 +44,60 @@ mozqabdLogoImage.src = 'images/mozqabdlogo.png'
 
 //function that generates the banner preview from user input text;
 function previewBanner() {
-  background_ctx.clearRect(0, 0, background.width, background.height); //            Clearing all
-  textLayer_ctx.clearRect(0, 0, textLayer.width, textLayer.height);  //                 layers
-  fxLogoLayer_ctx.clearRect(0,0,fxLogoLayer.width, textLayer.height); //                before
-  mozqabdLogoLayer_ctx.clearRect(0,0, mozqabdLogoLayer.width, textLayer.height); //     preview
 
-  background_ctx.drawImage(backgroundImage, 0, 0, background.width, background.height);
-  fxLogoLayer_ctx.drawImage(fxLogoImage,105,110, fxLogoLayer.height/2.2, fxLogoLayer.height/2.2);
-  mozqabdLogoLayer_ctx.drawImage(mozqabdLogoImage, 763,285, fxLogoImage.height/4.3197, fxLogoImage.height/7.623);
-  textLayer_ctx.textAlign = "center";
-  textLayer_ctx.fillText(document.getElementById("banner_heading").value, textLayer.width/2, textLayer.height/4);
+    background_ctx.clearRect(0, 0, background.width, background.height); //            Clearing all
+    textLayer_ctx.clearRect(0, 0, textLayer.width, textLayer.height);  //                 layers
+    fxLogoLayer_ctx.clearRect(0,0,fxLogoLayer.width, textLayer.height); //                before
+    mozqabdLogoLayer_ctx.clearRect(0,0, mozqabdLogoLayer.width, textLayer.height); //     preview
 
-  //now drwaing the textLayer canvas into the background canvas
-  background_ctx.drawImage(textLayer, 0, 0, background.width, background.height);
-  //drawing fx logo layer
-  background_ctx.drawImage(fxLogoLayer, 0, 0, background.width, background.height);
-  background_ctx.drawImage(mozqabdLogoLayer,0,0, background.width, background.height);
+    // Drawing background
+    background_ctx.drawImage(backgroundImage, 0, 0, background.width, background.height);
+
+    //now drwaing the textLayer canvas into the background canvas
+    textLayer_ctx.textAlign = "center";
+    textLayer_ctx.fillText(document.getElementById("banner_heading").value, textLayer.width/2, textLayer.height/4);
+    background_ctx.drawImage(textLayer, 0, 0, background.width, background.height);
+
+    //drawing fx logo layer
+    if(channel.value>=0 && channel.value<=3)   // changing fxlogo based on selected channel
+    fxLogoLayer_ctx.drawImage(fxLogoImage[channel.value],105,110, fxLogoLayer.height/2.2, fxLogoLayer.height/2.2);
+    else {          // if no channel is selected, default is release
+        fxLogoLayer_ctx.drawImage(fxLogoImage[0],105,110, fxLogoLayer.height/2.2, fxLogoLayer.height/2.2);
+    }
+    background_ctx.drawImage(fxLogoLayer, 0, 0, background.width, background.height);
+
+    //drawing mozqabd community logo layer
+    mozqabdLogoLayer_ctx.drawImage(mozqabdLogoImage, 763,285, fxLogoImage[0].height/4.3197, fxLogoImage[0].height/7.623);
+    background_ctx.drawImage(mozqabdLogoLayer,0,0, background.width, background.height);
 }
+
 //Will it look good to lead the canvas totally white? Nope, show some default banner background;
 window.addEventListener("load", function(){
-  background_ctx.drawImage(backgroundImage, 0, 0, background.width, background.height);
-  fxLogoLayer_ctx.drawImage(fxLogoImage,105,110, fxLogoLayer.height/2.2, fxLogoLayer.height/2.2);
-  mozqabdLogoLayer_ctx.drawImage(mozqabdLogoImage, 763,285, fxLogoImage.height/4.3197, fxLogoImage.height/7.623);
-  background_ctx.drawImage(fxLogoLayer, 0, 0, background.width, background.height);
-
+    background_ctx.drawImage(backgroundImage, 0, 0, background.width, background.height);
+    fxLogoLayer_ctx.drawImage(fxLogoImage[0],105,110, fxLogoLayer.height/2.2, fxLogoLayer.height/2.2);
+    mozqabdLogoLayer_ctx.drawImage(mozqabdLogoImage, 763,285, fxLogoImage[0].height/4.3197, fxLogoImage[0].height/7.623);
+    background_ctx.drawImage(fxLogoLayer, 0, 0, background.width, background.height);
+    background_ctx.drawImage(mozqabdLogoLayer, 0, 0, background.width, background.height);
 });
+
+
+//update banner preview based on channel selection
+channel.addEventListener("change", function(){
+    previewBanner(); //
+});
+
 //Calling the friendly previewBanner function;
 var preview = document.getElementById("preview");
 preview.addEventListener("click", previewBanner);
 
-
 //function that downloads the previously generated banner;
 function downloadImage(link, canvasID, fileName) {
-  link.href = canvasID.toDataURL('image/png'+'==');
-  link.download = fileName;
+    link.href = canvasID.toDataURL('image/png'+'==');
+    link.download = fileName;
 }
 
 var downloadButton = document.getElementById("download");
 downloadButton.addEventListener("click", function(){
-  previewBanner(); //downloading gets the exact current state by this;
-  downloadImage(this, background, "TestdayBanner.png");
+    previewBanner(); //downloading gets the exact current state by this;
+    downloadImage(this, background, "TestdayBanner.png");
 });
